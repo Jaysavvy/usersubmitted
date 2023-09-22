3d5ea6b477f1093e46f3d00e5bb69212779aaed1
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-09-22 09:46:31 -0400

    fixed button issue

--- src/App.tsx
+++ src/App.tsx
@@ -62,10 +62,11 @@ function App() {
         <label className="block mb-1 text-sm font-medium text-grey" htmlFor="floating_password">Password</label>
         <input type="password" id="password" {...register("password")} />
         </div>
+        <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shawdow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full">Submit Here</button>
         </form>
         <DevTool control={control}/>
 
-        <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shawdow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full">Submit Here</button>
+        
 
       </div>
     </div>
